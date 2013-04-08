465e653e0fd19ecd82ec8d9b23512443195fbdd7
Miracle.J <gujunjia@gmail.com>
2013-04-08 10:09:42 +0800

    init

---
+++ baroque/baroque/BQViewController.h
@@ -0,0 +1,13 @@
+//
+//  BQViewController.h
+//  baroque
+//
+//  Created by Sn Dev on 13-4-8.
+//  Copyright (c) 2013年 Baroque. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BQViewController : UIViewController
+
+@end
---
+++ baroque/baroque/BQViewController.m
@@ -0,0 +1,29 @@
+//
+//  BQViewController.m
+//  baroque
+//
+//  Created by Sn Dev on 13-4-8.
+//  Copyright (c) 2013年 Baroque. All rights reserved.
+//
+
+#import "BQViewController.h"
+
+@interface BQViewController ()
+
+@end
+
+@implementation BQViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
