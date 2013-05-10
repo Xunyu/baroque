006f86773f2d1d8f0caaebc7cafef6be5e1801f9
Elric Wang <elric.wwj@gmail.com>
2013-05-10 16:34:18 +0800

    add mocked menu api

---
+++ api-server/src/main/java/com/baroque/api/web/BaseServlet.java
@@ -0,0 +1,22 @@
+package com.baroque.api.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: elric
+ * Date: 13-5-10
+ * Time: 下午4:08
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class BaseServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+}
