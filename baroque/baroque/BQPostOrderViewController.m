70f9743c7e4c25e210d70c9043768248bd7b304b
Xunyu <xunyucn@gmail.com>
2013-05-15 11:21:35 +0800

    delete redundancy

--- baroque/baroque/BQPostOrderViewController.m
+++ baroque/baroque/BQPostOrderViewController.m
@@ -23,18 +23,6 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 - (IBAction)backButtonTapped:(id)sender {
     [self dismissModalViewControllerAnimated:YES];
 }
